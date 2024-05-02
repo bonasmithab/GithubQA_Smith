d6590634f23b8ecaab47ad54c89587d758adcf27
“Smitha” <bonasmitha.behura@gmail.com>
2024-05-02 11:50:27 +0200

    version1

---
+++ src/main/java/GitHubExample.java
@@ -0,0 +1,11 @@
+public class GitHubExample {
+
+    public static void main(String[] args) {
+        //Codes for version1
+
+        int x = 1;
+        int y = 2;
+        System.out.println(x+y);
+        System.out.println("Done with codes for version1");
+    }
+}
