a97b98b51901d6e7a157b94f2b627396aa25de90
“Smitha” <bonasmitha.behura@gmail.com>
2024-05-03 15:22:38 +0200

    v10

---
+++ src/main/java/testExcercise1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class testExcercise1 {
+    public static void main(String[] args) {
+        //Addition, Subtraction, Multiplication, Division and remainder Calculation
+        //        Create a simple Calculator that does:
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter two numbers for calculation: ");
+        double num1 = input.nextDouble();
+        double num2 = input.nextDouble();
+        double result;
+        String c =
+
+        if(c == "+"){
+            System.out.println(num1 + num2);
+        }
+
+    }
+}
